563d7d511c372e04260962681de860059411fa87
Rakurai96 <balbianello96@gmail.com>
2021-03-26 12:24:01 +0100

    all tests passed

--- src/main/java/com/codecool/Challenge.java
+++ src/main/java/com/codecool/Challenge.java
@@ -3,6 +3,15 @@ package com.codecool;
 public class Challenge {
 
     public static int minPositive(int a, int b) {
-
+        if (0 < a) {
+            if(0 < b) {
+                return a<b?a:b;
+            } else {
+                return a;
+            }
+        } else if (0 < b) {
+            return b;
+        }
+        return -1;
     }
 }
